2cac1de56f2b0bd67f5d710b7c17bb67e4fadea2
Esteban Alvarado <estalvgs1999@estudiantec.cr>
2021-12-01 12:22:51 -0600

    request_response_body

--- main.py
+++ main.py
@@ -1,7 +1,30 @@
-from fastapi import FastAPI
+# Python
+from typing import Optional
+
+# Pydantic
+from pydantic import BaseModel
+
+# FastAPI
+from fastapi import FastAPI,Body
 
 app = FastAPI()
+
+# Models
+class Person(BaseModel):
+    first_name: str
+    last_name: str
+    age: int
+    hair_color: Optional[str] = None
+    is_married: Optional[bool] = None
+
 
 @app.get("/")
 def home():
     return { "Hello": "World" }
+
+
+# Request and Response Body
+# (...) -> required param
+@app.post("/person/new")
+def create_person(person: Person = Body(...)):
+    return person
